5090835223ca7d008962e99a5d389bdb5e04e952
Sergio Castillo <chechocastillo2710@gmail.com>
2026-02-17 16:25:21 -0500

    update excersises semana 1

---
+++ semana_4/excersise_1.py
@@ -0,0 +1,19 @@
+my_first_int=20
+my_second_int= 8
+result= my_first_int + my_second_int
+print(result)
+
+my_first_int=20
+my_first_float=8.9
+result= my_first_int + my_first_float
+print(result)
+
+other_string= "deporte"
+f_string= f"a mi me gusta hacer {other_string}"
+print(f_string)
+
+other_string= "correr minimo"
+second_string= " 5 veces por semana"
+f_string= f"a mi me gusta {other_string  + second_string}"
+print(f_string)
+      
